3425562e4e2c600df5314c391988dcaeff955921
Besermenji <besermenji.vladimir@gmail.com>
2017-05-28 00:22:46 +0200

    Redux logger added for development.

--- src/store.js
+++ src/store.js
@@ -1,6 +1,12 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from  './reducers/rootReducer';
+import logger from 'redux-logger';
+
+let middlewares = [];
+if (process.env.NODE_ENV === `development`) {
+  middlewares.push(logger);
+}
 
 export default(initialState) => {
-    return createStore(rootReducer, initialState);
+  return createStore(rootReducer, initialState, applyMiddleware(...middlewares));
 }
