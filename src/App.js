84631687e4795ae1f29f886063dece55792eee19
Besermenji <besermenji.vladimir@gmail.com>
2017-05-27 23:59:25 +0200

    Router added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import logo from './logo.svg';
 import './App.css';
 
@@ -13,6 +14,12 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p><Link to="/login">Home</Link></p>
+
+        <p><Link to="/contact">Contact</Link></p>
+
+        <p><Link to="/notexist">Not Exist</Link></p>
+        {this.props.children}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import Store from './store';
+import Routes from './routes';
 
 const StoreInstance = Store();
+const history = syncHistoryWithStore(browserHistory, StoreInstance);
 
 ReactDOM.render(
   <Provider store={StoreInstance}>
-    <App />
+    <Routes history={history} />
   </Provider>
   , document.getElementById('root'));
 registerServiceWorker();
