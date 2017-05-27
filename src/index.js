9238214dea3de90254867c0f9e2830d3f728d0c0
Besermenji <besermenji.vladimir@gmail.com>
2017-05-27 22:49:40 +0200

    react create app, redux added

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+import Store from './store';
+
+const StoreInstance = Store();
+
+ReactDOM.render(
+  <Provider store={StoreInstance}>
+    <App />
+  </Provider>
+  , document.getElementById('root'));
+registerServiceWorker();
