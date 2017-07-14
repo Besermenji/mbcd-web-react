a9d007669701d34e7d272c9eb1f040c698023768
besermenji <besermenji.vladimir@gmail.com>
2017-07-14 20:58:08 +0200

    Api url now in env variable.

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,10 +1,13 @@
 import { CALL_API } from 'redux-api-middleware'
 import * as actions from '../constants/auth.js'
+import {
+  apiUrl
+} from '../constants/general'
 
 export const login = (data) => ({
   [CALL_API]: {
     types: [actions.LOGIN, actions.LOGIN_SUCCESS, actions.LOGIN_FAILURE],
-    endpoint: 'http://localhost:3010/v1/auth_user',
+    endpoint: `${apiUrl}/v1/auth_user`,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
@@ -14,7 +17,7 @@ export const login = (data) => ({
 export const register = (data) => ({
   [CALL_API]: {
     types: [actions.REGISTER, actions.REGISTER_SUCCESS, actions.REGISTER_FAILURE],
-    endpoint: 'http://localhost:3010/v1/users',
+    endpoint: `${apiUrl}/v1/users`,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
---
+++ src/constants/general.js
@@ -0,0 +1,2 @@
+const envVariable = 'REACT_APP_MBCD_FRONT_API_URL'
+export const apiUrl = process.env[envVariable]
