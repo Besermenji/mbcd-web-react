7bed5fa730f10eba88bf243986da2e663d30b2ff
besermenji <besermenji.vladimir@gmail.com>
2017-07-14 23:10:47 +0200

    Revert

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -7,7 +7,7 @@ import {
 export const login = (data) => ({
   [CALL_API]: {
     types: [actions.LOGIN, actions.LOGIN_SUCCESS, actions.LOGIN_FAILURE],
-    endpoint: `${apiUrl}v1/auth_user`,
+    endpoint: `${apiUrl}/v1/auth_user`,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
@@ -17,7 +17,7 @@ export const login = (data) => ({
 export const register = (data) => ({
   [CALL_API]: {
     types: [actions.REGISTER, actions.REGISTER_SUCCESS, actions.REGISTER_FAILURE],
-    endpoint: `${apiUrl}v1/users`,
+    endpoint: `${apiUrl}/v1/users`,
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
